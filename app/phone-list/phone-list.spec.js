9c3e52e06afa832e05241631a5e41b4bdc985ed5
Ortega Lopez <554662@CTS.COM>
2016-10-28 13:12:16 +0200

    fixed testing for component

--- app/phone-list/phone-list.spec.js
+++ app/phone-list/phone-list.spec.js
@@ -1,12 +1,10 @@
 describe('phoneList', function(){
   beforeEach(module('phoneList'));
   describe('PhoneListController', function() {
-    it('should create a `phones` model with 3 phones', inject(function($controller) {
-      var scope = {};
-      var ctrl = $controller('PhoneListController as PhoneCtrl', {$scope: scope});
-
-      expect(scope.PhoneCtrl.phones.length).toBe(3);
-      expect(scope.PhoneCtrl.name).toBe('World');
+    it('should create a `phones` model with 3 phones', inject(function($componentController) {
+      var ctrl = $componentController('phoneList');
+      expect(ctrl.phones.length).toBe(3);
+      expect(ctrl.name).toBe('World');
     }));
   });
 });

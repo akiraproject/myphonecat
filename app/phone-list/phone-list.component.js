3ad274c72b6bdf80beb373ed456eb515777d034c
Ortega Lopez <554662@CTS.COM>
2016-10-28 12:52:58 +0200

    More modularity on components

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -7,23 +7,28 @@ angular
   .module('phoneList')
   .component('phoneList',{
     templateUrl: 'phone-list/phone-list.template.html',
-      controller: function PhoneListController(){
-        var vm=this;
-        vm.name="World";
-        vm.phones=[
-          {
-            name: 'Nexus S',
-            snippet: 'Fast just got faster with Nexus S.'
-          }, {
-            name: 'Motorola XOOM™ with Wi-Fi',
-            snippet: 'The Next, Next Generation tablet.'
-          }, {
-            name: 'MOTOROLA XOOM™',
-            snippet: 'The Next, Next Generation tablet.'
-          }
-        ];
-      }
+      controller: PhoneListController
   });
+
+
+
+
+  function PhoneListController(){
+    var vm=this;
+    vm.name="World";
+    vm.phones=[
+      {
+        name: 'Nexus S',
+        snippet: 'Fast just got faster with Nexus S.'
+      }, {
+        name: 'Motorola XOOM™ with Wi-Fi',
+        snippet: 'The Next, Next Generation tablet.'
+      }, {
+        name: 'MOTOROLA XOOM™',
+        snippet: 'The Next, Next Generation tablet.'
+      }
+    ];
+  }
 
 
 })();

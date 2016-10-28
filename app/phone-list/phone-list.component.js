7c42c6e4287f3252769ee1c7f07cff331370eaaf
Ortega Lopez <554662@CTS.COM>
2016-10-28 15:36:48 +0200

    Sorting with select input

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -16,15 +16,19 @@ angular
     vm.phones=[
       {
         name: 'Nexus S',
-        snippet: 'Fast just got faster with Nexus S.'
+        snippet: 'Fast just got faster with Nexus S.',
+        age: 1
       }, {
         name: 'Motorola XOOM™ with Wi-Fi',
-        snippet: 'The Next, Next Generation tablet.'
+        snippet: 'The Next, Next Generation tablet.',
+        age: 2
       }, {
         name: 'MOTOROLA XOOM™',
-        snippet: 'The Next, Next Generation tablet.'
+        snippet: 'The Next, Next Generation tablet.',
+        age: 3
       }
     ];
+    vm.orderProp = 'age';
   }
 
 
--- app/phone-list/phone-list.spec.js
+++ app/phone-list/phone-list.spec.js
@@ -1,10 +1,27 @@
 describe('phoneList', function(){
+
+    // Load the module that contains the `phoneList` component before each test
   beforeEach(module('phoneList'));
+
+  // Test the controller
   describe('PhoneListController', function() {
-    it('should create a `phones` model with 3 phones', inject(function($componentController) {
-      var ctrl = $componentController('phoneList');
+    var ctrl;
+
+    beforeEach(inject(function($componentController) {
+     ctrl = $componentController('phoneList');
+   }));
+
+    it('should create a `phones` model with 3 phones', function() {
       expect(ctrl.phones.length).toBe(3);
+    });
+
+    it('should create a Hello World message', function() {
       expect(ctrl.name).toBe('World');
-    }));
+    });
+
+    it('should set a default value for the orderProp model', function(){
+      expect(ctrl.orderProp).toBe('age');
+    });
+
   });
 });
